3deea64ac759b5cb8e46bc81df4f0438ea1fbde3
safdar211ali <safdar211@hotmail.com>
2024-08-24 19:28:19 +0500

    updated

--- abdyeing/ab_dyeing/doctype/program/program.py
+++ abdyeing/ab_dyeing/doctype/program/program.py
@@ -2,18 +2,19 @@ import frappe
 from frappe.model.document import Document
 
 class Program(Document):
-    def on_submit(self):
-        so = frappe.new_doc("Sales Order")
-        so.customer = self.customer
-        so.transaction_date = self.date
-        so.order_type = 'Sales'
-        for item in self.program_item:
-            it = so.append("items", {})
-            it.item_code = item.construction
-            it.qty = item.qty
-            it.delivery_date = self.date
-
-        try:
-            so.save()
-        except Exception as e:
-            frappe.throw(str(e))
+    pass
+    # def on_submit(self):
+    #     so = frappe.new_doc("Sales Order")
+    #     so.customer = self.customer
+    #     so.transaction_date = self.date
+    #     so.order_type = 'Sales'
+    #     for item in self.program_item:
+    #         it = so.append("items", {})
+    #         it.item_code = item.construction
+    #         it.qty = item.qty
+    #         it.delivery_date = self.date
+    #
+    #     try:
+    #         so.save()
+    #     except Exception as e:
+    #         frappe.throw(str(e))
