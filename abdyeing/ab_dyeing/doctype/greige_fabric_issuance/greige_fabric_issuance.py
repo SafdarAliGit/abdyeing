1f8c8afdc80c0ad445a39755acf234cfbfb2b8d1
safdar211ali <safdar211@hotmail.com>
2024-08-29 23:57:07 +0500

    updated

--- abdyeing/ab_dyeing/doctype/greige_fabric_issuance/greige_fabric_issuance.py
+++ abdyeing/ab_dyeing/doctype/greige_fabric_issuance/greige_fabric_issuance.py
@@ -13,7 +13,7 @@ class GreigeFabricIssuance(Document):
         se.posting_date = self.date
         source_warehouse = self.source_warehouse
         se.ref_doctype = 'Greige Fabric Issuance'
-        se.ref_no =  self.name
+        se.ref_no = self.name
         for item in self.greige_fabric_issuance_item:
             se.append("items", {
                 "s_warehouse": source_warehouse,
@@ -34,10 +34,18 @@ class GreigeFabricIssuance(Document):
         try:
             se.submit()
             for item in self.greige_fabric_issuance_item:
-                batch = frappe.get_doc("Batch", item.fabric_lot_no)
-                if batch:
-                    batch.g_lot_no = item.fabric_lot_no if item.fabric_lot_no else None
-                batch.save()
+                s_batch = frappe.get_doc("Batch", item.fabric_lot_no)
+                if s_batch:
+                    s_batch.g_lot_no = item.fabric_lot_no if item.fabric_lot_no else None
+                s_batch.save()
+
+            for item in self.greige_fabric_issuance_item:
+                t_batch = frappe.get_doc("Batch", item.m_lot_no)
+                if t_batch:
+                    t_batch.quality = s_batch.quality if s_batch.quality else None
+                    t_batch.construction = s_batch.construction if s_batch.construction else None
+                    t_batch.greige_width = s_batch.greige_width if s_batch.greige_width else None
+                t_batch.save()
 
         except Exception as e:
             frappe.throw(str(e))
